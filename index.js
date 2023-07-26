6915473626c82dc2f4469e6708d3b6bdb5e1e890
Braux <83966759+brauxx@users.noreply.github.com>
2023-07-26 10:27:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
-const inputField = document.getElementById('input');
-const chatMessages = document.getElementById('chat-messages');
-const submitButton = document.getElementById('submit');
+const inputField = document.getElementById("input");
+const chatMessages = document.getElementById("chat-messages");
+const loadingContainer = document.getElementById("loading-container");
 let newMessage = null;
-let userId = getCookie('userId');
+let userId = getCookie("userId");
 
 // If the userId is not set, generate a new one
 if (!userId) {
-  userId = 'website-user-' + Math.random().toString(36).substring(2, 15);
-  setCookie('userId', userId);
+  userId = "website-user-" + Math.random().toString(36).substring(2, 15);
+  setCookie("userId", userId);
 }
 
-inputField.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
+inputField.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
     sendMessage();
   }
 });
 
 function sendMessage() {
+  // Hide the input field and show the loading spinner
+  loadingContainer.style.display = "block";
+  loadingContainer.style.animation = "fadeIn 0.5s ease forwards";
+  inputField.style.animation = "fadeOut 0.5s ease forwards";
+  inputField.disabled = true;
+
   const query = inputField.value;
 
   const data = {
     text: query,
-    key: '178a0827-6a3f-41a9-b3a3-c22e01ef0cf6',
+    key: "178a0827-6a3f-41a9-b3a3-c22e01ef0cf6",
     user_id: userId,
     speak: false
   };
 
-  fetch('https://api.carterlabs.ai/chat', {
-    method: 'POST',
+  fetch("https://api.carterlabs.ai/chat", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(data)
   })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Input:', data.input);
-      console.log('Output:', data.output);
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Input:", data.input);
+      console.log("Output:", data.output);
 
       // Create a new message element
-      newMessage = document.createElement('div');
-      newMessage.classList.add('message');
+      newMessage = document.createElement("div");
+      newMessage.classList.add("message");
       newMessage.textContent = data.output.text;
 
       // Append the message element to the chat messages container
@@ -51,8 +57,8 @@ function sendMessage() {
 
       // Fade in the message element
       setTimeout(() => {
-        newMessage.style.display = 'block';
-        const messages = document.querySelectorAll('.message');
+        newMessage.style.display = "block";
+        const messages = document.querySelectorAll(".message");
         if (messages.length > 1) {
           messages[messages.length - 2].style.opacity = 0;
           setTimeout(() => {
@@ -60,21 +66,34 @@ function sendMessage() {
           }, 1000);
         }
       }, 100);
+
+      // Hide the loading spinner and show the input field again
+      // Clear the input field
+      inputField.value = "";
+
+      // Show the input field with a fade-in animation
+      inputField.style.animation = "fadeIn 0.5s ease forwards";
+      inputField.disabled = false;
+
+      // Hide the loading container with a fade-out animation
+      loadingContainer.style.animation = "fadeOut 0.5s ease forwards";
     })
-    .catch(error => {
-      console.error('Error:', error);
+    .catch((error) => {
+      console.error("Error:", error);
+
+      // Hide the loading spinner and show the input field again
     });
 
   // Clear the input field
-  inputField.value = '';
+  inputField.value = "";
 }
 
 // Function to get the value of a cookie
 function getCookie(name) {
-  const cookies = document.cookie.split(';');
+  const cookies = document.cookie.split(";");
   for (let i = 0; i < cookies.length; i++) {
     const cookie = cookies[i].trim();
-    if (cookie.startsWith(name + '=')) {
+    if (cookie.startsWith(name + "=")) {
       return cookie.substring(name.length + 1);
     }
   }
@@ -85,3 +104,91 @@ function getCookie(name) {
 function setCookie(name, value) {
   document.cookie = name + "=" + value + "; path=/";
 }
+
+const input = document.querySelector("#input");
+const minWidthBlurred = 240;
+const minWidthFocused = 300;
+const padding = 8; // Adjust this value if you have any padding on the input element
+
+// Function to calculate the text width based on content
+function getTextWidth(text) {
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  context.font = window.getComputedStyle(input).getPropertyValue("font");
+  const width = context.measureText(text).width;
+  canvas.remove();
+  return width + padding * 2; // Add padding to the calculated width
+}
+
+// Function to set the input field width
+function setInputWidth() {
+  const currentText = input.value;
+  const textWidth = getTextWidth(currentText);
+  const maxWidth = window.innerWidth * 0.8;
+
+  // Adjust the input field width based on content
+  if (input === document.activeElement) {
+    // If the input field is focused, set the width to minWidthFocused
+    input.style.width = `${Math.max(textWidth, minWidthFocused)}px`;
+  } else {
+    // If the input field is not focused, set the width to minWidthBlurred
+    input.style.width = `${Math.max(textWidth, minWidthBlurred)}px`;
+  }
+
+  // If the width exceeds maxWidth, let it wrap to the next line
+  if (textWidth >= maxWidth) {
+    input.style.whiteSpace = "pre-wrap";
+    // Limit the width of the input field to maxWidth
+    input.style.width = `${maxWidth}px`;
+  } else {
+    input.style.whiteSpace = "nowrap";
+  }
+}
+
+// Add event listeners to adjust the input field width dynamically
+input.addEventListener("input", setInputWidth);
+input.addEventListener("focus", setInputWidth);
+input.addEventListener("blur", setInputWidth);
+
+// Function to set the input field height
+function setInputHeight() {
+  const currentText = input.value;
+  const textWidth = getTextWidth(currentText);
+  const maxWidth = window.innerWidth * 0.8;
+  const lineHeight = parseInt(
+    window.getComputedStyle(input).getPropertyValue("line-height")
+  );
+
+  // Adjust the input field height based on content
+  if (input === document.activeElement) {
+    // If the input field is focused, set the height to lineHeight
+    input.style.height = `${lineHeight}px`;
+  } else {
+    // If the input field is not focused, set the height to lineHeight
+    input.style.height = `${lineHeight}px`;
+  }
+
+  // If the width exceeds maxWidth, increase the height to wrap to the next line
+  if (textWidth >= maxWidth) {
+    const lines = Math.ceil(textWidth / maxWidth);
+    input.style.height = `${lines * lineHeight}px`;
+  }
+}
+
+// Add event listeners to adjust the input field height dynamically
+input.addEventListener("input", setInputHeight);
+input.addEventListener("focus", setInputHeight);
+input.addEventListener("blur", setInputHeight);
+
+const textarea = document.querySelector("textarea");
+
+// Function to set the textarea height
+function setTextareaHeight() {
+  // Reset the textarea height to auto to calculate the scroll height
+  textarea.style.height = "auto";
+  // Set the textarea height to its scroll height
+  textarea.style.height = `${textarea.scrollHeight}px`;
+}
+
+// Add event listeners to adjust the textarea height dynamically
+textarea.addEventListener("input", setTextareaHeight);
